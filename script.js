c213976f6c200a3f04ce04393549562cde324ee1
MikeMaille88 <mickael.bohrer@gmail.com>
2023-07-25 12:13:05 +0200

    correct infos writing page 2 bottom

--- script.js
+++ script.js
@@ -29,11 +29,30 @@ const getPlanetPrez = () => {
   .then (response => response.json())
   .then((data) => {
     let p = document.getElementById("planet");
+    let p2 = document.getElementById("planet2");
     let d = document.getElementById("desc");
+    let l = document.getElementById("language");
+    let a = document.getElementById("accomodation");
+    let c = document.getElementById("currencies");
+    let s = document.getElementById("see");
+    let r = document.getElementById("residents");
+    let t = document.getElementById("traveler");
+    let locImg = document.getElementById("locationImage");
+    let locName = document.getElementById("locationName");
       for (let i=0; i<data.length; i++){
           if (currentPlanet == data[i].name){
-            p.innerHTML = data[i].name;
+            p.innerHTML = data[i].name.toUpperCase();
+            p2.innerHTML = data[i].name.toUpperCase();
             d.innerHTML = data[i].description;
+            locImg.src = data[i].image;
+            locImg.alt = `Image ${data[i].name}`;
+            locName.innerHTML = data[i].location.toUpperCase();
+            l.innerHTML = `Langue(s) : ${data[i].language}`;
+            a.innerHTML = `Hébergement(s) : ${data[i].accomodation}`;
+            c.innerHTML = `Monnaie(s) : ${data[i].currencies}`;
+            s.innerHTML = `A voir : ${data[i].see}`;
+            r.innerHTML = `Population(s) : ${data[i].residents}`;
+            t.innerHTML = `Pour les voyageurs : ${data[i].traveler}`;
             getPlanetData(data[i].id);
           }
       }
